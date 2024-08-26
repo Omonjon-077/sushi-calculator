6f251082fdc9006786664b138f22e2263f1b3e42
Omonjon <omonjons077@gmail.com>
2024-08-26 18:22:42 +0500

    Site bags fixed and new functions added

--- script/main.js
+++ script/main.js
@@ -5,18 +5,62 @@ if ($('#phone-num').length) {
             mask: '+{998}(00)000-00-00'
         }
     );
-};
+}
+
+// script for validate form
+if ($('.needs-validation').length) {
+    (() => {
+        'use strict'
+
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        const forms = document.querySelectorAll('.needs-validation')
+
+        // Loop over them and prevent submission
+        Array.from(forms).forEach(form => {
+            form.addEventListener('submit', event => {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+
+                form.classList.add('was-validated')
+            }, false)
+        })
+    })()
+}
 
 // Function for phone number field
 if ($('#phone-num').length) {
     function validateForm() {
         let x = document.querySelector('#phone-num').value;
         if (x.length <= 16) {
-            alert("Telefon raqamangizni to'liq kiriting");
+            alert("Telefon raqamingizni to'liq kiriting!");
             return false;
         }
     }
-};
+}
+
+// Function for promo check
+if ($('#promocode').length) {
+    let promoInput = document.querySelector('#promocode');
+    function checkPromo() {
+        if (promoInput.value == '') {
+            alert(`Iltimos promokodni kiriting!`)
+        } else if (promoInput.value == activePromo.name) {
+            let totalPriceEl = document.querySelector('.total-price'),
+                activeTotalPrice = document.querySelector('.active-total-price');
+            document.querySelector('.active-total').classList.remove('d-none');
+            document.querySelector('.cart-total .text-danger').classList.add('text-line-through');
+            totalPriceEl.classList.add('text-line-through');
+            let totalPrice = totalPriceEl.innerText;
+            totalPrice -= activePromo.price;
+            activeTotalPrice.innerText = totalPrice;
+        } else {
+            alert(`Siz bunday promokod yutib olmagansiz yoki nato'g'ri kiritgansiz!`)
+        }
+    }
+}
+
 
 // Function for toasts when click btn
 const toastTrigger = document.getElementById('liveToastBtn')
--- script/quiz.js
+++ script/quiz.js
@@ -1,144 +1,145 @@
 activeTest = localStorage.getItem('test') ? localStorage.getItem('test') : null;
-activePromo = localStorage.getItem('promo') ? localStorage.getItem('promo') : null;
+activePromo = JSON.parse(localStorage.getItem('promo')) ? JSON.parse(localStorage.getItem('promo')) : null;
 userScore = localStorage.getItem('score') ? localStorage.getItem('score') : null;
-if (activeTest === 'active') {
-    document.querySelector('.test-box').classList.add('d-none');
-    document.querySelector('.header').classList.add('d-none');
-    document.querySelector('.warning-block').classList.remove('d-none');
-    document.querySelector('.result').innerText = `Siz 5 ta savoldan ${userScore} tasini topgansiz`;
-} else {
-    document.querySelector('.warning-block').classList.add('d-none');
-}
-if (activePromo){
-    document.querySelector('.have-promo').classList.remove('d-none');
-    document.querySelector('.have-promo span').innerText = activePromo;
-}
-
-const quizData = [
-    {
-        question: "Qaysi rangni yoqtiradi ?",
-        a: "Sariq",
-        b: "Oq",
-        c: "Yashil",
-        d: "Qora",
-        correct: "b"
-    },
-    {
-        question: "Qaysi mobil o'yinni sevib o'ynaydi?",
-        a: "Mobile Legends",
-        b: "PUBG",
-        c: "UNO",
-        d: "Brawl Stars",
-        correct: "b"
-    },
-    {
-        question: "Qaysi maktabda o'qigan?",
-        a: "26",
-        b: "40",
-        c: "1",
-        d: "2",
-        correct: "c"
-    },
-    {
-        question: "Qaysi kinoni yoqtiradi?",
-        a: "Interstellar",
-        b: "Muhabbat masofasi",
-        c: "Jon Uik",
-        d: "Uzuklar hukumdori",
-        correct: "b"
-    },
-    {
-        question: "Qaysi kino kompaniyani yoqtiradi?",
-        a: "MARVEL",
-        b: "DC",
-        c: "20th century fox",
-        d: "Paramount Pictures",
-        correct: "a"
-    },
-    {
-        question: "Qaysi multfilmni yoqtiradi yoqtiradi?",
-        a: "Hayvonlar shahri",
-        b: "Kung Fu Panda",
-        c: "Tom and Jerry",
-        d: "Shrek",
-        correct: "a"
-    },
-    {
-        question: "Qaysi davlatga sayohat qilishni hohlaydi?",
-        a: "AQSH",
-        b: "Yaponiya",
-        c: "Italiya",
-        d: "Angliya",
-        correct: "b"
-    },
-    {
-        question: "Qaysi telefon ishlab chiqaradigan kompaniyani yoqtiradi?",
-        a: "Apple",
-        b: "Samsung",
-        c: "Xiaomi",
-        d: "Oppo",
-        correct: "a"
-    },
-    {
-        question: "Qaysi notebook turini yoqtiradi?",
-        a: "MacBook",
-        b: "Asus Rog",
-        c: "Asus Vivobook",
-        d: "Asus Zenbook",
-        correct: "a"
-    },
-    {
-        question: "Qaysi taxallus bilan ko'p o'zini ataydi?",
-        a: "SooM",
-        b: "Gidra",
-        c: "Pandora",
-        d: "Sayfer",
-        correct: "d"
-    },
-    {
-        question: "Kitob o'qishni yoqtiradimi?",
-        a: "Yo'q",
-        b: "Ha",
-        c: "Bilmadim",
-        d: "Ajoyib savol",
-        correct: "b"
-    },
-    {
-        question: "Qaysi mashina kompaniyasini yoqtiradi?",
-        a: "BMW",
-        b: "Mercedes Benz",
-        c: "Porsche",
-        d: "Audi",
-        correct: "b"
-    },
-    {
-        question: "Qaysi fudbol o'yinchisini yoqtiradi?",
-        a: "Ronaldo",
-        b: "Messi",
-        c: "Mpabbe",
-        d: "Neymar",
-        correct: "a"
-    },
-    {
-        question: "Qaysi fudbol klubini yoqtiradi?",
-        a: "Barcelona",
-        b: "Manchester City",
-        c: "Manchester United",
-        d: "Real Madrid",
-        correct: "d"
+if ($('.questions-page').length) {
+    if (activeTest === 'active') {
+        document.querySelector('.test-box').classList.add('d-none');
+        document.querySelector('.header').classList.add('d-none');
+        document.querySelector('.warning-block').classList.remove('d-none');
+        document.querySelector('.result').innerText = `Siz 5 ta savoldan ${userScore} tasini topgansiz`;
+    } else {
+        document.querySelector('.warning-block').classList.add('d-none');
     }
-];
+    if (activePromo) {
+        document.querySelector('.have-promo').classList.remove('d-none');
+        document.querySelector('.have-promo span').innerText = activePromo.name;
+    }
 
-const quiz = document.getElementById('quiz');
+    const quizData = [
+        {
+            question: "Qaysi rangni yoqtiradi ?",
+            a: "Sariq",
+            b: "Oq",
+            c: "Yashil",
+            d: "Qora",
+            correct: "b"
+        },
+        {
+            question: "Qaysi mobil o'yinni sevib o'ynaydi?",
+            a: "Mobile Legends",
+            b: "PUBG",
+            c: "UNO",
+            d: "Brawl Stars",
+            correct: "b"
+        },
+        {
+            question: "Qaysi maktabda o'qigan?",
+            a: "26",
+            b: "40",
+            c: "1",
+            d: "2",
+            correct: "c"
+        },
+        {
+            question: "Qaysi kinoni yoqtiradi?",
+            a: "Interstellar",
+            b: "Muhabbat masofasi",
+            c: "Jon Uik",
+            d: "Uzuklar hukumdori",
+            correct: "b"
+        },
+        {
+            question: "Qaysi kino kompaniyani yoqtiradi?",
+            a: "MARVEL",
+            b: "DC",
+            c: "20th century fox",
+            d: "Paramount Pictures",
+            correct: "a"
+        },
+        {
+            question: "Qaysi multfilmni yoqtiradi yoqtiradi?",
+            a: "Hayvonlar shahri",
+            b: "Kung Fu Panda",
+            c: "Tom and Jerry",
+            d: "Shrek",
+            correct: "a"
+        },
+        {
+            question: "Qaysi davlatga sayohat qilishni hohlaydi?",
+            a: "AQSH",
+            b: "Yaponiya",
+            c: "Italiya",
+            d: "Angliya",
+            correct: "b"
+        },
+        {
+            question: "Qaysi telefon ishlab chiqaradigan kompaniyani yoqtiradi?",
+            a: "Apple",
+            b: "Samsung",
+            c: "Xiaomi",
+            d: "Oppo",
+            correct: "a"
+        },
+        {
+            question: "Qaysi notebook turini yoqtiradi?",
+            a: "MacBook",
+            b: "Asus Rog",
+            c: "Asus Vivobook",
+            d: "Asus Zenbook",
+            correct: "a"
+        },
+        {
+            question: "Qaysi taxallus bilan ko'p o'zini ataydi?",
+            a: "SooM",
+            b: "Gidra",
+            c: "Pandora",
+            d: "Sayfer",
+            correct: "d"
+        },
+        {
+            question: "Kitob o'qishni yoqtiradimi?",
+            a: "Yo'q",
+            b: "Ha",
+            c: "Bilmadim",
+            d: "Ajoyib savol",
+            correct: "b"
+        },
+        {
+            question: "Qaysi mashina kompaniyasini yoqtiradi?",
+            a: "BMW",
+            b: "Mercedes Benz",
+            c: "Porsche",
+            d: "Audi",
+            correct: "b"
+        },
+        {
+            question: "Qaysi fudbol o'yinchisini yoqtiradi?",
+            a: "Ronaldo",
+            b: "Messi",
+            c: "Mpabbe",
+            d: "Neymar",
+            correct: "a"
+        },
+        {
+            question: "Qaysi fudbol klubini yoqtiradi?",
+            a: "Barcelona",
+            b: "Manchester City",
+            c: "Manchester United",
+            d: "Real Madrid",
+            correct: "d"
+        }
+    ];
 
-function getRandomElements(arr, count) {
-    return arr.sort(() => 0.5 - Math.random()).slice(0, count);
-}
+    const quiz = document.getElementById('quiz');
 
-const randomElements = getRandomElements(quizData, 5);
+    function getRandomElements(arr, count) {
+        return arr.sort(() => 0.5 - Math.random()).slice(0, count);
+    }
 
-randomElements.forEach((item, index) => {
-    quiz.innerHTML += `
+    const randomElements = getRandomElements(quizData, 5);
+
+    randomElements.forEach((item, index) => {
+        quiz.innerHTML += `
                 <div class="question">
                     <p class="mb-1 fs-4 fw-semibold fst-italic">${index + 1}. ${item.question}</p>
                     <input type="radio" required id="q${index}_a" name="q${index}" value="a">
@@ -151,27 +152,28 @@ randomElements.forEach((item, index) => {
                     <label class="" for="q${index}_d">${item.d}</label>
                 </div>
             `;
-});
-
-function submitQuiz() {
-    let score = 0;
-    randomElements.forEach((item, index) => {
-        const answer = document.querySelector(`input[name="q${index}"]:checked`);
-        if (answer && answer.value === item.correct) {
-            score++;
-        }
     });
-    if (score >= 3) {
-        document.querySelector('.promo').classList.remove('d-none');
-        document.querySelector('.promo span').innerText = randomElement.name;
-        localStorage.setItem('promo', randomElement.name);
+
+    function submitQuiz() {
+        let score = 0;
+        randomElements.forEach((item, index) => {
+            const answer = document.querySelector(`input[name="q${index}"]:checked`);
+            if (answer && answer.value === item.correct) {
+                score++;
+            }
+        });
+        if (score >= 3) {
+            document.querySelector('.promo').classList.remove('d-none');
+            document.querySelector('.promo span').innerText = randomElement.name;
+            localStorage.setItem('promo', JSON.stringify(randomElement));
+        }
+        if (score >= 1) {
+            setTimeout(function () {
+                location.reload();
+            }, 3000);
+            localStorage.setItem('test', 'active');
+            localStorage.setItem('score', score)
+        }
+        document.getElementById('result').innerText = `Siz ${randomElements.length} ta savoldan ${score} ta topdingiz`;
     }
-    if (score >= 1) {
-        setTimeout(function() {
-            location.reload();
-        }, 3000);
-        localStorage.setItem('test', 'active');
-        localStorage.setItem('score', score)
-    }
-    document.getElementById('result').innerText = `Siz ${randomElements.length} ta savoldan ${score} ta topdingiz`;
 }
