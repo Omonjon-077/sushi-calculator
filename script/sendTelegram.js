f0269dd19695b4524c327ffe83edc912bfec9de1
Omonjon <omonjons077@gmail.com>
2024-09-18 11:32:15 +0500

    Site price send telegram and products function added

--- script/sendTelegram.js
+++ script/sendTelegram.js
@@ -11,12 +11,39 @@ let bot = {
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    let userName = document.getElementById("user-name"),
-        userPhone = document.getElementById("phone-num"),
-        userLocation = document.getElementById("user-location"),
-        userMessage = document.getElementById("user-massage");
+    let userName = document.querySelector("#user-name"),
+        userPhone = document.querySelector("#phone-num"),
+        userLocation = document.querySelector("#user-location"),
+        userMessage = document.querySelector("#user-massage");
 
-    let sendMessage = `Mijoz %0A Ismi: ${userName.value} %0A Telefon raqami: ${userPhone.value} %0A Manzili: ${userLocation.value} %0A Xabari: ${userMessage.value} %0A %0A`
+    // 1. Cartdagi barcha itemlarni olish
+    const cartItems = document.querySelectorAll('.cart-item');
+
+    // 2. Ma'lumotlarni arrayga yig'ish
+    const itemArray = Array.from(cartItems).map(item => {
+        const itemName = item.querySelector('.cart-item__title').textContent;
+        const itemQuantity = item.querySelector('.items__current').textContent;
+        return `  ${itemName}, ${itemQuantity} ta`; // Tartibli format
+    });
+
+    // 3. Arrayni bitta qatorda matn sifatida chiqarish
+    const resultText = itemArray.join('%0A'); // itemlarni "%0A (Enter)" bilan ajratamiz
+
+    // Umumiy summani olish
+    const totalPriceItem = document.querySelector('.total-price'),
+        promoTotalPriceItem = document.querySelector('.active-total');
+
+    let totalPrice;
+    if (promoTotalPriceItem.classList.contains('d-none')) {
+        totalPrice = totalPriceItem.textContent;
+    } else {
+        totalPrice = promoTotalPriceItem.querySelector('.active-total-price').textContent;
+    }
+
+    let sendMessage = `
+    Mijoz %0A 👤 Ismi: ${userName.value} %0A 📞 Telefon raqami: ${userPhone.value} %0A 📍 Manzili: ${userLocation.value} %0A 💬 Xabari: ${userMessage.value} 
+    %0A %0ATovarlar: %0A${resultText} %0A %0A💵 Umumiy narx: ${totalPrice} so'm
+    `
 
     if (userPhone.value.length <= 16) {
         alert("Telefon raqamingizni to'liq kiriting ☎");
